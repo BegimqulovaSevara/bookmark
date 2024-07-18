92095847160c8f47c33300e60042de077b836bbd
BegimqulovaSevara <sevarabegimkulova58@gmail.com>
2024-07-18 23:06:53 +0500

    Add tab panels js code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,33 @@
+const elsTabItem = document.querySelectorAll('.tabs__item');
+const elsTabPanel = document.querySelectorAll('.tabs__panel');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
+
+function detactivateTabItems () {
+    elsTabItem.forEach( function (elTabItem) {
+        elTabItem.classList.remove('tabs__item--active')
+    });
+};
+
+function detactivateTabPanels () {
+    elsTabPanel.forEach( function (elTabPanel) {
+        elTabPanel.classList.remove('tabpanels__item--active');
+    });
+};
 
 elsTabLink.forEach( (elTabLink) =>{
     elTabLink.addEventListener( 'click', function(evt) {
-        evt.preventDefault()
-    })
-})
+        evt.preventDefault();
+        
+        detactivateTabItems();
+
+        elTabLink.parentElement.classList.add('tabs__item--active');
+
+        detactivateTabPanels();
+
+        // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`)
+        const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget)
+
+        elTargetPanel.classList.add('tabpanels__item--active')
+
+    });
+});
